f9dfd36983b51580327f3ff17e78c3bcf8af78da
danma <dong5053@naver.com>
2024-06-19 15:47:00 +0900

    로고 수정

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -34,7 +34,7 @@ function Header() {
             <Navbar collapseOnSelect expand="lg" className='custom-navbar'>
                 <Container>
                     <Navbar.Brand href="/" className={isDarkMode ? "dark-mode" : ""}>
-                    SlowCatch!
+                    QuickCatch~~!
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
